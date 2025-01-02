920d187aadd77fdb6fa2777baf01a781ce0c2e34
simon <schapman14663@gmail.com>
2025-01-02 11:49:03 +0000

    check calling CLI is functional

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+//use std::path::PathBuf;
+use clap::{arg, Command};
+
+fn main(){
+    let _m = Command::new("seaports")
+        .version("0.0.1")
+        .about("to be done")
+        .subcommand_required(true)
+        .arg_required_else_help(true)
+        .subcommand(
+            Command::new("new")
+            .about("Create New Project")
+            .arg(arg!(<LANGUAGE> "The main language to use in the new project"))
+            .arg_required_else_help(true),
+            );
+
+    println!("Hello World, seaports command is functional");
 }
